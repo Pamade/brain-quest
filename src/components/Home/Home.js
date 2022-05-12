35e14d85aafd9d2b2172c4e1d1d7f854c21c4f1a
Pamade <insune15@gmail.com>
2022-05-12 19:02:13 +0200

    fix video

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,25 +9,63 @@ function Home() {
           <h2>Remember Number</h2>
           <p className="home__description">Remember the biggest number</p>
           <p className="home__preview-p">Preview</p>
-          <video autoPlay muted loop className="home__video">
-            <source
-              src="https://cdn.kapwing.com/final_627d12741e27fd0066682598_349636.mp4"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
+          <div
+            style={{
+              width: "100%",
+              height: "0",
+              position: "relative",
+              paddingBottom: "19.219%",
+            }}
+            className="home__video"
+          >
+            <iframe
+              src="https://streamable.com/e/5dsn8p?autoplay=1&nocontrols=1"
+              frameBorder="0"
+              width="100%"
+              height="100%"
+              allowFullScreen
+              allow="autoplay"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: "0px",
+                top: "0px",
+                overflow: "hidden",
+              }}
+            ></iframe>
+          </div>
         </div>
         <div className="home__box">
           <h2>Where it was</h2>
           <p className="home__description">Try to guess location of elements</p>
           <p className="home__preview-p">Preview</p>
-          <video autoPlay muted loop className="home__video">
-            <source
-              src="https://cdn.kapwing.com/2022_05_12_16_00_44-17TLQe2N5-.mp4"
-              type="video/mp4"
-            />
-            Your browser does not support the video tag.
-          </video>
+          <div
+            style={{
+              width: "100%",
+              height: "0",
+              position: "relative",
+              paddingBottom: "19.219%",
+            }}
+            className="home__video"
+          >
+            <iframe
+              src="https://streamable.com/e/6ef8ec?autoplay=1&nocontrols=1"
+              frameBorder="0"
+              width="100%"
+              height="100%"
+              allowFullScreen
+              allow="autoplay"
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                left: "0px",
+                top: "0px",
+                overflow: "hidden",
+              }}
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
